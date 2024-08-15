859a83a25595172b083e908391185ce8ce3229e1
Artsiom Bukhautsou <artem.bukhavtsov@gmail.com>
2024-08-15 14:50:20 +0200

    tables for users decks and translations

--- db/translations.sql
+++ db/translations.sql
@@ -21,3 +21,24 @@ CREATE TABLE IF NOT EXISTS public.users (
     );
 
 CREATE INDEX idx_user_name ON users (user_name);
+
+-- Create decks table
+CREATE TABLE IF NOT EXISTS public.decks (
+    id SERIAL PRIMARY KEY,
+    deck_name VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL REFERENCES public.users(id) ON DELETE CASCADE
+);
+
+-- Create index on user_id column in decks table
+CREATE INDEX idx_user_id_deck ON decks (user_id);
+
+-- Create deck_translations join table
+CREATE TABLE IF NOT EXISTS public.deck_translations (
+    id SERIAL PRIMARY KEY,
+    deck_id INT NOT NULL REFERENCES public.decks(id) ON DELETE CASCADE,
+    translation_id INT NOT NULL REFERENCES public.translations(id) ON DELETE CASCADE
+);
+
+-- Create index on deck_id and translation_id columns in deck_translations table
+CREATE INDEX idx_deck_id ON deck_translations (deck_id);
+CREATE INDEX idx_translation_id ON deck_translations (translation_id);
